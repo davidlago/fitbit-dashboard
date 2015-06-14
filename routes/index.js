4a6aa5fea14de56a7d9e66229be4a9c286d6e75b
David Lago <davidlago@gmail.com>
2015-06-14 18:29:42 -0400

    Adding linters

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 // Read config file based on environment
-var config = require('../config.json')[process.env.NODE_ENV||'development'];
+var config = require('../config.json')[process.env.NODE_ENV || 'development'];
 // Substitute API key and secret from environment
-config.grant.fitbit.key = process.env.FITBIT_API_KEY
-config.grant.fitbit.secret = process.env.FITBIT_API_SECRET
+config.grant.fitbit.key = process.env.FITBIT_API_KEY;
+config.grant.fitbit.secret = process.env.FITBIT_API_SECRET;
 
 // Configure Purest for API access
 var Purest = require('purest');
 var fitbit = new Purest({
-  provider:'fitbit',
+  provider: 'fitbit',
   key: config.grant.fitbit.key,
   secret: config.grant.fitbit.secret
 });
@@ -24,12 +24,12 @@ router.get('/', function(req, res) {
 router.get('/callback', function(req, res) {
   fitbit
     .query()
-    .get("user/-/profile")
+    .get('user/-/profile')
     .auth(
       req.session.grant.response.access_token,
       req.session.grant.response.access_secret
     )
-    .request(function (err, res2, body) {
+    .request(function(err, res2, body) {
       // Render user profile
       res.render('profile', {profile: body.user});
     });
